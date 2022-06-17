ae8f32fdb6e0fe2d6071dbcead4231c0c996f4c0
Sameer Singh <s690558576@gmail.com>
2022-06-18 00:22:12 +0530

    quadratic equation -v1

---
+++ quadraticequation.js
@@ -0,0 +1,30 @@
+let a = 45;
+let b = 89;
+let c = 23;
+
+let discriminant = (b*b) - 4*a*c;
+
+if(discriminant > 0)
+{
+    let root1 = (-b + Math.sqrt(discriminant))/2*a;
+    let root2 = (-b - Math.sqrt(discriminant))/2*a;
+    console.log(`${root1},,${root2}`)
+}
+
+else if(discriminant == 0)
+{
+    let root = -b/2*a;
+    console.log(root);
+}
+
+else
+{
+    let realPart = (-b / (2 * a)).toFixed(2);
+    let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+
+    // result
+    console.log(
+    `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`);
+}
+
+
