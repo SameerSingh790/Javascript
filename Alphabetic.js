4226c84172b532c2f6e4e4d5f6b682be3edd4f78
Sameer Singh <s690558576@gmail.com>
2022-06-18 01:41:02 +0530

    Alphabetic order -v1

---
+++ Alphabetic.js
@@ -0,0 +1,9 @@
+let Name = "Sameer";
+
+let Name_Array = Name.split('');
+let SORT = Name_Array.sort()
+
+const After_SORT = SORT.join('')
+
+console.log(`Before Sort = ${Name}`)
+console.log(`After Sort = ${After_SORT}`)
---
+++ ReverseString.js
@@ -0,0 +1,7 @@
+let Sentence = "GOOD WORK, Rahul"
+
+let Array_1 = Sentence.split('');
+let Reverse_String = Array_1.reverse();
+
+let result = Reverse_String.join('');
+console.log(result);
