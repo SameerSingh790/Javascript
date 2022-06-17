9e01180307737f59925b566172ce1bd5fcbbdde0
Sameer Singh <s690558576@gmail.com>
2022-06-18 02:04:27 +0530

    odd or even v1

---
+++ oddoreven.js
@@ -0,0 +1,12 @@
+const odd_or_Even = (NUM)=>{
+    if(NUM % 2 == 0 ){
+        return console.log(`${NUM} is Even`)
+    }
+    else{
+        return console.log(`${NUM} is odd`)
+    }
+}
+
+odd_or_Even(3)
+
+///https://www.programiz.com/javascript/examples
