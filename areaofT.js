bb2f81ad846b9521087fcca810197087372f62a4
Sameer Singh <s690558576@gmail.com>
2022-06-17 16:32:54 +0530

    Area of all types of traingle v3

--- areaofT.js
+++ areaofT.js
@@ -1,5 +1,29 @@
-const Base = 12;
-const height = 45;
+// const Base = 12;
+// const height = 45;
 
-result = (Base * height)/2;
-console.log(result);
+// result = (Base * height)/2;
+// console.log(result);
+
+const square = (S1,S2,S3)=>{
+    if(S2 == null && S3 == null){
+        const result = (Math.sqrt(3)/4)*S1*S1;
+        return Math.round(result)
+    }
+
+    if(S3 == null)
+    {
+        const result = (S1 * S2)/2;
+        return Math.round(result);
+    }
+
+    else{
+        const S = (S1+S2+S3)/2
+        const result = Math.sqrt(S*(S-S1)*(S-S2)*(S-S3));
+        return Math.round(result);
+    }
+
+}
+
+console.log(square(45))
+console.log(square(45,56))
+console.log(square(45,78,62))
