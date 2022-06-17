ba9b21c19029e07225e5ad46c4feddf21db62667
Sameer Singh <s690558576@gmail.com>
2022-06-18 00:26:02 +0530

    Quadratic Adding function -V2

--- quadraticequation.js
+++ quadraticequation.js
@@ -1,20 +1,18 @@
-let a = 45;
-let b = 89;
-let c = 23;
 
+const Quadratic = (a,b,c)=>{
 let discriminant = (b*b) - 4*a*c;
 
 if(discriminant > 0)
 {
     let root1 = (-b + Math.sqrt(discriminant))/2*a;
     let root2 = (-b - Math.sqrt(discriminant))/2*a;
-    console.log(`${root1},,${root2}`)
+   return console.log(`${root1},,${root2}`)
 }
 
 else if(discriminant == 0)
 {
     let root = -b/2*a;
-    console.log(root);
+    return console.log(root);
 }
 
 else
@@ -23,8 +21,9 @@ else
     let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
 
     // result
-    console.log(
-    `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`);
+    return console.log(`The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`);
+}
 }
 
+Quadratic(11,5,20)
 
