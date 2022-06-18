269af168c4f6f82d6316a91a2f4b235dd9bc1dab
Sameer Singh <s690558576@gmail.com>
2022-06-19 00:57:57 +0530

    array or not 'add function 'v2

--- arrornot.js
+++ arrornot.js
@@ -1,5 +1,20 @@
-let arr = [1,2,3,4,5,6];
-let arr1 = 45;
+// let arr = [1,2,3,4,5,6];
+// let arr1 = 45;
 
-console.log(Array.isArray(arr))
-console.log(Array.isArray(arr1))
+// console.log(Array.isArray(arr))
+// console.log(Array.isArray(arr1))
+
+//or using function
+
+const ARRAY_or_NOT = (A)=>{
+    if(Array.isArray(A)){
+        return console.log(`Is array`)
+    }
+    else{
+        return console.log(`Not an array`)
+    }
+}
+
+ARRAY_or_NOT([4,6,2,4,7,9,5]);
+ARRAY_or_NOT(4)
+
