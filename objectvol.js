a7066700059001dfd34e0c68ea1c028417a16bdf
Sameer Singh <s690558576@gmail.com>
2022-06-23 16:33:22 +0530

    create objects using functions adn prototypes

--- objectvol.js
+++ objectvol.js
@@ -1,10 +1,15 @@
-const Cylinder = {
-    radius : 12,
-    height : 15
+
+function Cylinder(cyl_height,cyl_diameter){
+    this.cyl_height = cyl_height;
+    this.cyl_diameter = cyl_diameter;
 }
 
-const Pi = Math.PI
-const r2 = Cylinder.radius**2;
+Cylinder.prototype.Volume = function(){
+    const radius = this.cyl_diameter/2;
+    return Math.PI * radius * radius * this.cyl_height;
+}
 
-const Volume = Pi * r2 * Cylinder.height;
-console.log(Volume)
+const cyl = new Cylinder(7,9)
+
+console.log(cyl)
+console.log(cyl.Volume().toFixed(4))
